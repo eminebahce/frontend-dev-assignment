ef7663b044132227fa5b1281c71fc4a0edbf620d
EmineBC <eminebahce@gmail.com>
2019-04-10 13:49:15 +0200

    final changes added

--- src/Components/Suggestions.js
+++ src/Components/Suggestions.js
@@ -3,7 +3,10 @@ import React from 'react';
 export default function Suggestions (props) {
     return(
         <ul data-test="suggestions">
-            {props.suggestionsList && props.suggestionsList.map((item, i) => <li key={i}>{item.searchterm} {item.nrResults}</li>)}
+            {props.suggestionsList &&
+            props.suggestionsList.map((item, index) =>
+                <li key={index}>{item.searchterm} ({item.nrResults})</li>
+            )}
         </ul>
     )
 };
